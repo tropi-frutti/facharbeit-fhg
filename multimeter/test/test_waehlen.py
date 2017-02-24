52a09b939221043487d42f8efd2dc09d6e76f616
steinorb <steinorb@DEL0138.ppal.directory>
2017-02-24 16:06:55 +0100

    simplify import

--- multimeter/test/test_waehlen.py
+++ multimeter/test/test_waehlen.py
@@ -3,15 +3,14 @@ Created on 24.02.2017
 
 @author: steinorb
 '''
-import unittest.mock
+import unittest
 import waehlen
-import builtins
 
 
 class WaehlenTest(unittest.TestCase):
 
 
-    @unittest.mock.patch('builtins.input')
+    @unittest.mock.patch('builtins.input') # mock the builtin input() method
     def testAuswahl(self, mock_input):
         mock_input.return_value = "1"
         auswahl = waehlen.auswahl()
