634ac372d7bf1bb4fca160f0003fe30489b0ded1
Steiner <norbertsteiner@siemens-ppal.com>
2017-02-24 16:02:24 +0100

    made unit test running

--- multimeter/test/test_waehlen.py
+++ multimeter/test/test_waehlen.py
@@ -5,16 +5,17 @@ Created on 24.02.2017
 '''
 import unittest.mock
 import waehlen
+import builtins
 
 
 class WaehlenTest(unittest.TestCase):
 
 
-    @unittest.mock.patch('__builtin__.input')
+    @unittest.mock.patch('builtins.input')
     def testAuswahl(self, mock_input):
         mock_input.return_value = "1"
         auswahl = waehlen.auswahl()
-        self.assertEqual(auswahl, "1")
+        self.assertEqual(auswahl, 1)
 
 
 if __name__ == "__main__":
