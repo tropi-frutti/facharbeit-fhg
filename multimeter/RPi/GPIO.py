8b87fdc6cd37503cb139bdca0db6b08224705005
steinorb <steinorb@DEL0138.ppal.directory>
2017-02-24 11:05:43 +0100

    manage requirements

---
+++ multimeter/RPi/GPIO.py
@@ -0,0 +1,29 @@
+'''
+Created on 24.02.2017
+
+Mockup for development
+
+@author: steinorb
+'''
+BOARD = 1
+OUT = 1
+IN = 1
+BCM = 1
+
+def setmode(a):
+    print(a)
+   
+def setup(a, b):
+    print(a, b)
+   
+def output(a, b):
+    print(a, b)
+   
+def input(a):
+    return 0
+
+def cleanup():
+    pass
+   
+def setwarnings(flag):
+    pass
