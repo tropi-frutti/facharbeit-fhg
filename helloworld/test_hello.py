d90279b840af81fd608895c2345bd60fadb8a306
steinorb <steinorb@DEL0138.ppal.directory>
2017-02-24 08:53:02 +0100

    testprojekt hinzu gefügt

---
+++ helloworld/test_hello.py
@@ -0,0 +1,18 @@
+'''
+Created on 24.02.2017
+
+@author: steinorb
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+
+    def testHello(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testHello']
+    unittest.main()
